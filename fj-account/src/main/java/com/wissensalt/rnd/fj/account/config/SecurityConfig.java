da5f6ebae2ce1a536ad5ad404c562a019311648f
ACHMAD FAUZI <fauzi.knightmaster.achmad@gmail.com>
2019-04-26 11:31:58 +0700

    change config

--- fj-account/src/main/java/com/wissensalt/rnd/fj/account/config/SecurityConfig.java
+++ fj-account/src/main/java/com/wissensalt/rnd/fj/account/config/SecurityConfig.java
@@ -7,16 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * Created on 3/5/19.
@@ -31,7 +28,7 @@ public class SecurityConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.resourceId("oc-resource");
+        resources.resourceId("fauzi-jenius-resource");
     }
 
     @Bean
